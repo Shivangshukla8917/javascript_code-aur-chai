a863d49d67be63513ef959e8bb7f92efd92baec1
Shivangshukla8917 <135588982+Shivangshukla8917@users.noreply.github.com>
2025-03-25 19:53:55 +0000

    conversions

---
+++ _basics/03_typeconversion.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+//null => object =>0
+//undefined =>undefined=>NaN
+//CONSOLE.LOG(SCORE)
+
+let nums = Number(score)
+console.log(nums)
+
+// "33" =>33
+// "33abc" => NaN
+//true =>1; false =>0
+
+let loogenIn = 1
+let isitloggenIn = Boolean(loogenIn)
+console.log(isitloggenIn)
+
+// 1 =>true ; 0+.false
+//"" =>false
+//"shivang" =>true

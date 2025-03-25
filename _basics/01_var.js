334f9bf4afdd506ffa7e917de1023a7cd4e12b58
Shivangshukla8917 <135588982+Shivangshukla8917@users.noreply.github.com>
2025-03-25 18:47:04 +0000

    new topic variables

---
+++ _basics/01_var.js
@@ -0,0 +1,10 @@
+const name =  "shivang"
+let email = "shivangshukla@gmsil.com"
+var rollno = "2210013155053"
+let state;
+
+email = "shivsundar@gmail.com"
+//name = sundaramnot allowd//
+console.log(name)
+console.table([name,email,rollno,state])
+//prefer not to use var bcoz of issue of block scope and functional scope//

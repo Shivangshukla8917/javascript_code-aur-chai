dba8725032a7cf4e9f0a31011acead359e2c3b2c
Shivangshukla8917 <135588982+Shivangshukla8917@users.noreply.github.com>
2025-03-26 12:32:36 +0000

    adding some operations

--- _basics/03_typeconversion.js
+++ _basics/03_typeconversion.js
@@ -3,17 +3,43 @@ let score = "33abc"
 //undefined =>undefined=>NaN
 //CONSOLE.LOG(SCORE)
 
-let nums = Number(score)
-console.log(nums)
+// let nums = Number(score)
+// console.log(nums)
 
 // "33" =>33
 // "33abc" => NaN
 //true =>1; false =>0
 
-let loogenIn = 1
-let isitloggenIn = Boolean(loogenIn)
-console.log(isitloggenIn)
+// let loogenIn = 1
+// let isitloggenIn = Boolean(loogenIn)
+// console.log(isitloggenIn)
 
 // 1 =>true ; 0+.false
 //"" =>false
 //"shivang" =>true
+
+
+//*****************operations ****************/
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);//-3
+
+// let str1 = "shivang"
+// let str2 = "shukla"
+// let str3 = str1 + str2
+// console.log(str3);//shivangshukla
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");
+// console.log(1 + 2 +"2");//32
+// console.log("1" + 2 + 2);//122
+
+// console.log(true);//true
+// console.log(+true);//1
+// console.log(+"");// 0 => 1
+
+let counter = 100
+counter++
+console.log(counter);
+

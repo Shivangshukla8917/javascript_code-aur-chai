4fd5072908644b46813356a98bc521ff01e47b04
Shivangshukla8917 <135588982+Shivangshukla8917@users.noreply.github.com>
2025-03-27 12:54:00 +0000

    strings

---
+++ _basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "shivang"
+const repoCount = 50
+
+//console.log(name + repocount + "value");
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('shivang')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);

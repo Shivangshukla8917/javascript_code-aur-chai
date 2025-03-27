694cb74ca4812ed72bc7c8753e953f08b4384be0
Shivangshukla8917 <135588982+Shivangshukla8917@users.noreply.github.com>
2025-03-27 18:35:41 +0000

    prototype items

--- _basics/05_strings.js
+++ _basics/05_strings.js
@@ -5,7 +5,27 @@ const repoCount = 50
 
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('shivang')
+const gameName = new String('shiva-ng')
 
-console.log(gameName[0]);
+console.log(gameName[0]);//h
 console.log(gameName.__proto__);
+
+console.log(gameName.length);//string length
+console.log(gameName.toUpperCase());//convert string into uppercase
+console.log(gameName.charAt(4));//tell the character according to number(0=> length -1)
+console.log(gameName.indexOf('h'));//tell the index location
+console.log(gameName.substring(0,5));//print string from start to given index
+const newstr = '    shivang    '
+console.log(newstr.trim());//remove the extra space 
+const url = 'https//shivkasundar.com'
+console.log(url.replace('ka','-'));//it repaces the word from some other word
+//includes() => find the given word
+//split('_') => convert the word into array with using spicific separator
+
+
+
+
+
+
+
+

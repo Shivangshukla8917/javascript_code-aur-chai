eecb66f04948f9cba50e4c3f74575d086ec4aa3f
Shivangshukla8917 <135588982+Shivangshukla8917@users.noreply.github.com>
2025-03-28 09:21:03 +0000

    datesinfo

---
+++ _basics/07_datesin.js
@@ -0,0 +1,24 @@
+//Dates
+
+// let myDate = new Date();
+// console.log(myDate.toDateString());//fri mar 28 2025
+// console.log(myDate.toString());//fri mar 28 2025 06:44:13 GMT+0000
+// console.log(myDate.toISOString());//2025-03-28T06:45:55.983Z
+// console.log(myDate.toLocaleString());//3/28/2025, 6:45:55AM
+
+console.log(typeof myDate);//object
+
+
+// let myCreatedDate = new Date(2023,0,23,5,3)
+// console.log(myCreatedDate.toLocaleString());//1/23/2023,5:03:00 AM
+// let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myTimeStamp.getTime());
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default' , {
+    weekday:"long",
+}) 
+

9ee7cfd25acb358eccb54376ee62e91f4b28adec
Shivangshukla8917 <135588982+Shivangshukla8917@users.noreply.github.com>
2025-03-28 09:30:42 +0000

    methos in array

---
+++ _basics/02_basics/01_array1.js
@@ -0,0 +1,32 @@
+
+
+const myArr = [0,1,2,3,4,5]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr2);
+
+
+
+
+//ARRAY METHODS
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+myArr.unshift(0)
+myArr.shift()
+console.log(myArr.includes(2));//true
+console.log(myArr.indexOf(9));//-1
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr);
+//slice,splice
+
+
+
+
+
+
+
+
+console.log(myArr);

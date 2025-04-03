1b35b703262144aca09c181b53168abe4e665d60
Shivangshukla8917 <135588982+Shivangshukla8917@users.noreply.github.com>
2025-04-03 04:19:10 +0000

    arrow

--- _basics/14_arrow.js
+++ _basics/14_arrow.js
@@ -1,13 +1,48 @@
-const user = {
-    userName : "shivang",
-    price : 1000,
-    welcomeMessage: function() {
-        console.log(`${this.userName} , welcome to website`);
-        console.log(this);
-    }
+// const user = {
+//     userName : "shivang",
+//     price : 1000,
+//     welcomeMessage: function() {
+//         console.log(`${this.userName} , welcome to website`);
+//         console.log(this);
+//     }
+//}
+
+// const chai = function() {
+//     let userName = "shiv"
+//     console.log(this.userName);
+    
+// }
+
+const chai = () => {
+    let userName = "shivang"
+    console.log(this.userName);//undefined
+    
 }
+chai()
+
+
+
+
 // user.welcomeMessage()//shivang , welcome to website
 // user.userName ="sammy"
 // user.welcomeMessage()..sammy welcome to website
 
-console.log(this);//{}
+// console.log(this);//{}
+//^= only use in Object(this)
+
+
+
+ 
+
+
+/************************ EXPLICIT FUNCTION */
+// const addTwo = (num1 , num2) => {
+//     return num1 + num2
+// }
+
+/************************** IMPLICIT FUNCTION */
+// const addTwo = (num1 , num2) => (num1 +num2)
+
+const addTwo = (num1 , num2) => ({userName: "hitesh"})//using() to refer an object
+
+console.log(addTwo(3,4));

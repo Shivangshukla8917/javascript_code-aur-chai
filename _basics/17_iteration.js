1d762eeb5528b3a32e2791715d914d49ffff056c
Shivangshukla8917 <135588982+Shivangshukla8917@users.noreply.github.com>
2025-04-05 04:11:47 +0000

    for,while,do while looping

---
+++ _basics/17_iteration.js
@@ -0,0 +1,55 @@
+
+
+// for (let i = 0; i < 5; i++) {
+//     const ele = i;
+//     if(ele ==3) {
+//         console.log("3 is best number");
+        
+//     }
+//     console.log(element);
+    
+//     //12 3 is best number 345
+// }
+
+
+// for (let i = 0; i <= 6; i++) {
+//    console.log(`outer loop value : ${i}`);
+//    for (let j = 0; j < 6; j++) {
+//     // console.log(`inner loop value ${j} and inner loop${i}`);
+//     console.log(i +'*' + j + '=' + i*j);
+    
+    
+    
+//    }
+// }
+
+/***************************while loop */
+let index=0
+while (index <=10) {
+   console.log(`value of index is ${index}`);
+   index = index+2
+}
+/************************while statement */
+
+
+// let myarray = ['flash','batman','superman']
+// let arr = 0
+// while (arr < myarray.length) {
+//    console.log(`value is ${myarray[arr]}`);
+//    arr = arr+1
+   
+// }
+
+
+/******************do while statement */
+let score = 1
+
+do {
+   console.log(`score is ${score}`);
+   score++
+   
+   
+} while (score <= 10);
+
+
+

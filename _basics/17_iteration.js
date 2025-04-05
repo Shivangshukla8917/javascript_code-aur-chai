2faedc845cbe985ead983b88e55c68005014e6b3
Shivangshukla8917 <135588982+Shivangshukla8917@users.noreply.github.com>
2025-04-05 06:04:51 +0000

    for in done

--- _basics/17_iteration.js
+++ _basics/17_iteration.js
@@ -24,11 +24,11 @@
 // }
 
 /***************************while loop */
-let index=0
-while (index <=10) {
-   console.log(`value of index is ${index}`);
-   index = index+2
-}
+// let index=0
+// while (index <=10) {
+//    console.log(`value of index is ${index}`);
+//    index = index+2
+// }
 /************************while statement */
 
 
@@ -42,14 +42,64 @@ while (index <=10) {
 
 
 /******************do while statement */
-let score = 1
+// let score = 1
 
-do {
-   console.log(`score is ${score}`);
-   score++
+// do {
+//    console.log(`score is ${score}`);
+//    score++
    
    
-} while (score <= 10);
+// } while (score <= 10);
 
+/********************** ORDER and FILTER*/
+
+/*************array iteration */
+
+const arr = [1,2,3,4,5,6]
+for (const num of arr) {
+   console.log(num);
+}
+
+
+const greetings = 'shivangshukla'
+for (const greets of greetings) {
+   if (greets == 'n') {
+      console.log(`${greetings} done at shiva`);
+      break;
+      
+      
+   }
+   console.log(`each char is ${greets}`);
+}
+
+/*******************MAP  pe iteration yaha ni kr skte*/
+const map = new Map;//only unique value will be printed
+map.set('IN' , "INDIA")
+map.set('USA' , "united states of america")
+map.set('fr' , "france")
+// console.log(map);
+for(const [key,value] of map) {
+   console.log(key, '=' , value);//in = india
+   
+}
+
+/***************OBJECT ITERATION */
+const myObject = {
+   js : "javascript",
+   cpp : "cpp",
+   rb : "ruby",
+   swift : "swift by apple"
+}
+for (const key in object) {
+  console.log(`${key} shorcut for ${myObject[key]}`);
+  
+}
+
+/*********************using FOR IN in array */
+const program = ["do", "while", "py", "java"]
+for (const key in program) {
+  console.log(prgram[key]);//do while py java
+  console.log(key);//0123
+}
 
 
